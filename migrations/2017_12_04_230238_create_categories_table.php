a58e49484076ae0aae0fb9b5db897075cbf2412e
ross <hai30@126.com>
2017-12-05 00:01:45 +0800

    Update

---
+++ migrations/2017_12_04_230238_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->tinyInteger('enabled')->default(0)->comment('启用禁用 0:禁用 1:启用');
+
+            $table->string('name')->default('')->comment('分类名称');
+            $table->string('intro')->default('')->comment('分类简介');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
