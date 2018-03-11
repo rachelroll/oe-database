8912d61fd29882d69e76c6aae32f7df1cdf62809
ross <hai30@126.com>
2018-03-12 02:09:46 +0800

    Update products messages instructions

--- migrations/2017_12_02_192308_create_products_table.php
+++ migrations/2017_12_02_192308_create_products_table.php
@@ -18,14 +18,15 @@ class CreateProductsTable extends Migration
 
             $table->string('name')->default('')->comment('产品名称');
             $table->string('model')->default('')->comment('型号');
+            $table->string('buy_url')->default('')->comment('购买连接');
             $table->tinyInteger('cat_id')->default(0)->comment('分类ID');
             $table->tinyInteger('is_new')->default(0)->comment('是否新品 0:否 1:是');
             $table->tinyInteger('position')->default(0)->comment('产品在首页位置');
             $table->tinyInteger('sort')->default(0)->comment('产品排序');
             $table->tinyInteger('type')->default(0)->comment('产品级别 0:普通产品 1:主打产品 2:特色产品');
             $table->tinyInteger('enabled')->default(0)->comment('启用禁用 0:禁用 1:启用');
-            $table->string('cover')->default('')->comment('产品封面图');
-            $table->string('banner')->default('')->comment('产品Banner图');
+            $table->string('cover')->nullable()->comment('产品封面图');
+            $table->string('banner')->nullable()->comment('产品Banner图');
             $table->string('imgs')->default('')->comment('产品细节图');
             $table->string('video_img')->default('')->comment('视频封面图');
             $table->string('video_mp4')->default('')->comment('视频mp4');
@@ -36,8 +37,6 @@ class CreateProductsTable extends Migration
             $table->string('attr')->default('')->comment('产品属性');
             $table->integer('price')->default(0)->comment('价格(分)');
             $table->tinyInteger('rating')->default(0)->comment('推荐评级');
-
-
 
             $table->timestamps();
         });
---
+++ migrations/2018_03_11_224705_create_instructions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInstructionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('instructions', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('name')->default('')->comment('software instruction\'s name');
+            $table->tinyInteger('enabled')->default(0)->comment('enable or not');
+            $table->string('url')->default('')->comment('file uploaded url');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('instructions');
+    }
+}
