445cf47b71c41db99499384bc83211399499a6f2
rachel <hai30@163.com>
2018-06-28 17:03:14 +0800

    Update email

---
+++ migrations/2018_06_10_234726_create_emails_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emails', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('receiver')->comment('收件人');
+            $table->string('email')->comment('邮箱');
+            $table->tinyInteger('enabled')->default(0)->comment('启用禁用 0:禁用 1:启用');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emails');
+    }
+}
